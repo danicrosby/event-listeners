278ca97bd84740d56dfbf45d0c0f6a8ea68b9696
Dani Crosby <dani.crosby1111@gmail.com>
2021-01-26 18:34:28 -0600

    added bootstrap form

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 console.log("greetings, beautiful from main.js")
 
-
 const pies = [
   {
     name: 'Dutch Apple Pie',
@@ -63,3 +62,77 @@ const pies = [
     iceCream: 'none',
   },
 ];
+
+const printToDom = (divId, textToPrint) => {  // THIS FUNCTION TAKES IN THE ID AND TELLS IT WHAT TO DO// print to dom
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const pieBuilder = (obj) => {  //MAKE A FUNCTION TO LOOP THROUGH THE ARRAY
+  let domString = ''; // AND EVERYTIME IT LOOPS IT THE DOMSTRING GETS RESET
+  for (let i = 0; i < obj.length; i++) {
+    domString += `<div class="card my-2" style="width: 18rem;" id=${i}> 
+                    <div class="img-container" style="background-image: url('${obj[i].imageUrl}');"></div>
+                    <div class="card-body">
+                      <p class="card-text">${obj[i].name}</p>
+                      <p class="card-text">${obj[i].ingredients}</p>
+                      <p class="card-text">${obj[i].bakeTemp}</p>
+                      <p class="card-text">${obj[i].drinkPairing}</p>
+                      <p class="card-text">${obj[i].iceCream}</p>
+                      <button type="button" class="btn btn-danger" id="${i}">Delete</button>
+                    </div>
+                  </div>`;
+  }
+
+  printToDom('#pies', domString);  //THE PIEBUILDER FUNCTION CALLS THE PIES AND PRINTS EACH PIE TO THE DOM THROUGH THE DOMSTRING
+}
+
+const handleButtonClick = (e) => {  // A FUNCTION TO HANDLE EACH BUTTON CLICKED, WHICH BUTTON AND WHAT TO DO WHEN CLICKED
+  const buttonId = e.target.id;
+
+  // CHANGING BG COLOR BASED ON BUTTON CLICK
+  if (buttonId === 'Trinity') {
+    // DARK MODE
+    document.querySelector('body').style.backgroundColor = '#000';
+  } else if (buttonId === 'Doc') {
+    // LIGHT MODE
+    document.querySelector('body').style.backgroundColor = '#FFF';
+  } else if (buttonId === 'Aja') {
+    // MEDIUM MODE
+    document.querySelector('body').style.backgroundColor = '#808080';
+  } else if (buttonId === 'All') {
+    // DEFAULT
+    document.querySelector('body').style.backgroundColor = 'rgb(175, 196, 175)';
+  }
+
+  // UPDATE THE PIES BASED ON BUTTON CLICKED// LOOPING OVER PIES AND PUSHING A PIE TO AN INSTRUCTORS BUTTON
+  const selectedPies = [];
+  // pies[0].instructor // 'Doc' // STARTING AT POSITION [0], FOR LOOP AND DOT NOTATION TO PUSH OR ADD PIE TO INSTRUCTOR
+  for (let i = 0; i < pies.length; i++) {
+    if (pies[i].instructor === buttonId) {
+      selectedPies.push(pies[i]);
+    }
+  }
+
+  if (buttonId === 'All') {  // IF BUTTON ALL IS PRESSED THEN ALL THE PIES SHOW UP
+    // PRINT ALL THE PIES
+    pieBuilder(pies);
+  } else {
+    pieBuilder(selectedPies);
+  }
+
+}
+
+const buttonEvents = () => {   //EVENT LISTENERS ON EACH OF THE BUTTONS (ID'S)
+  document.querySelector('#All').addEventListener('click', handleButtonClick);
+  document.querySelector('#Doc').addEventListener('click', handleButtonClick);
+  document.querySelector('#Aja').addEventListener('click', handleButtonClick);
+  document.querySelector('#Trinity').addEventListener('click', handleButtonClick);
+}
+
+const init = () => {  // FUNCTION TO INITIALIZE THE WHOLE PAGE
+  buttonEvents();
+  pieBuilder(pies);
+}
+
+init();
